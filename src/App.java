25c6037912200d955a960bac9dbaf12aad54b439
AAGRcode <axel.gonzalez.280907@gmail.com>
2026-06-10 08:34:30 -0500

    PRACTICA TERMINADA

--- src/App.java
+++ src/App.java
@@ -1,14 +1,14 @@
-// import java.util.ArrayDeque;
-// import java.util.Deque;
-// import java.util.LinkedList;
-// import java.util.Queue;
-// import java.util.Stack;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        // runLinkedList();
-        // runQueue();
-        // runStack();
+        runLinkedList();
+        runQueue();
+        runStack();
 
         Ejercicio1 ejercicio1 = new Ejercicio1();
         String texto = "COMPUTACION";
@@ -22,64 +22,64 @@ public class App {
         System.out.println(ejercicio2.esPalindromo(palabra2));
     }
 
-    // private static void runStack() {
-    //     Stack<String> pila = new Stack<>();
-    //     pila.push("A");
-    //     pila.push("B");
-    //     pila.push("C");
-    //     String elemento = pila.pop();
-    //     System.out.println(pila.size());   
-    //     System.out.println(elemento);
+    private static void runStack() {
+        Stack<String> pila = new Stack<>();
+        pila.push("A");
+        pila.push("B");
+        pila.push("C");
+        String elemento = pila.pop();
+        System.out.println(pila.size());   
+        System.out.println(elemento);
 
-    //     Deque<String> pila2 = new ArrayDeque<>();
-    //     Deque<String> pila3 = new LinkedList<>();
-    //     pila2.push("a");
-    //     pila3.push("A");
-    //     pila.pop();
-    //     pila.pop();
+        Deque<String> pila2 = new ArrayDeque<>();
+        Deque<String> pila3 = new LinkedList<>();
+        pila2.push("a");
+        pila3.push("A");
+        pila.pop();
+        pila.pop();
 
-    // }
+    }
 
-    // private static void runQueue() {
-    //     Queue<String> cola = new ArrayDeque<>();
-    //     cola.offer("Jose");
-    //     cola.offer("Andres");
-    //     cola.offer("Ana");
-    //     System.out.println(cola.isEmpty());
-    //     System.out.println(cola.size());
+    private static void runQueue() {
+        Queue<String> cola = new ArrayDeque<>();
+        cola.offer("Jose");
+        cola.offer("Andres");
+        cola.offer("Ana");
+        System.out.println(cola.isEmpty());
+        System.out.println(cola.size());
         
-    //     System.out.println(cola.peek());
-    //     System.out.println(cola.size());
+        System.out.println(cola.peek());
+        System.out.println(cola.size());
 
-    //     System.out.println(cola.poll());
-    //     System.out.println(cola.size());
+        System.out.println(cola.poll());
+        System.out.println(cola.size());
 
-    //     while(!cola.isEmpty()){}
-    //         String cliente = cola.poll();
-    //         System.out.println("Atendi a " + cliente);
-    //     }
+        while(!cola.isEmpty()){
+            String cliente = cola.poll();
+            System.out.println("Atendi a " + cliente);
+        }
+    }
+        //offer -> agrega al final
+        //peek -> devuelve el primero sin eliminar
+        //poll -> devuelve el primero y lo elimina
 
-    //     //offer -> agrega al final
-    //     //peek -> devuelve el primero sin eliminar
-    //     //poll -> devuelve el primero y lo elimina
-
-    // private static void runLinkedList() {
-    //     System.out.println("Lista Enlazada");
-    //     LinkedList<String> nombres = new LinkedList<>();
-    //     System.out.println("Esta vacia? " + nombres.isEmpty());
-    //     System.out.println("Tamaño " + nombres.size());
+    private static void runLinkedList() {
+        System.out.println("Lista Enlazada");
+        LinkedList<String> nombres = new LinkedList<>();
+        System.out.println("Esta vacia? " + nombres.isEmpty());
+        System.out.println("Tamaño " + nombres.size());
         
-    //     nombres.add("Juan");
-    //     nombres.add("Pedro");
-    //     nombres.add("Maria");
-    //     nombres.add("Diego");
+        nombres.add("Juan");
+        nombres.add("Pedro");
+        nombres.add("Maria");
+        nombres.add("Diego");
 
-    //     System.out.println(nombres.getFirst());
-    //     System.out.println(nombres.get(2));
-    //     System.out.println(nombres.getLast());
+        System.out.println(nombres.getFirst());
+        System.out.println(nombres.get(2));
+        System.out.println(nombres.getLast());
 
 
-    //     System.out.println(nombres.peek());
-    //     System.out.println(nombres.size());
-    // }
+        System.out.println(nombres.peek());
+        System.out.println(nombres.size());
+    }
 }
